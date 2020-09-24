fa00c3af464c0c0311c6579540c489beb3986799
Victor Alvarado <alvaradoguzman79@hotmail.com>
2020-09-24 15:56:42 -0600

    Documentacion

--- SistemaDeMembresias1.0/src/com/conexion/Conexion.java
+++ SistemaDeMembresias1.0/src/com/conexion/Conexion.java
@@ -1,9 +1,15 @@
 package com.conexion;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import javax.swing.JOptionPane;
+
+/**
+ * Nombre de la clase: Conexion
+ * Fecha: 24-09-2020
+ * Versión: 1.0
+ * CopyRight: 
+ * @author Victor, Adrian, Andrea, Ever, Roberto
+ */
 
 public class Conexion {
     private Connection con;
@@ -15,7 +21,7 @@ public class Conexion {
     public boolean conectar() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bdsistemademembresias?", "root", "Itca123!");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bdsistemademembresias?", "root", "");
             return true;
         } catch (ClassNotFoundException | SQLException e) {
             JOptionPane.showMessageDialog(null,"Error al conectar "+e.getMessage(),"Error",0);
