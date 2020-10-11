60e544794d217ccf709e05076ea8374a90433100
victoralvarado <alvaradoguzman900@gmail.com>
2020-10-11 11:00:25 -0600

    Update DaoCarrito
    Format Code

--- SistemaDeMembresias1.0/src/com/dao/DaoCarrito.java
+++ SistemaDeMembresias1.0/src/com/dao/DaoCarrito.java
@@ -33,5 +33,4 @@ public class DaoCarrito extends Conexion implements OperacionesCarrito{
     public void eliminarCarrito(Carrito car) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
