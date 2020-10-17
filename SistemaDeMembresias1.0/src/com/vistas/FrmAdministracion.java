1487a91bcd92734f725fb8e4da2f5a6c2cdfd8d2
victoralvarado <alvaradoguzman900@gmail.com>
2020-10-17 10:56:10 -0600

    Create FrmAdministracion.java

---
+++ SistemaDeMembresias1.0/src/com/vistas/FrmAdministracion.java
@@ -0,0 +1,114 @@
+package com.vistas;
+
+/**
+ * Nombre de la clase: FrmAdministracion
+ * Fecha: 17-10-2020
+ * Versión: 1.0
+ * CopyRight: ITCA-FEPADE
+ * @author victor alvarado
+ */
+public class FrmAdministracion extends javax.swing.JFrame {
+
+    public FrmAdministracion() {
+        initComponents();
+        //Iniciar maximixado FrmAdministracion
+        this.setExtendedState(MAXIMIZED_BOTH);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        deskPnlContenedor = new javax.swing.JDesktopPane();
+        menuPrincipal = new javax.swing.JMenuBar();
+        btnMenu = new javax.swing.JMenu();
+        btnPrueba = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout deskPnlContenedorLayout = new javax.swing.GroupLayout(deskPnlContenedor);
+        deskPnlContenedor.setLayout(deskPnlContenedorLayout);
+        deskPnlContenedorLayout.setHorizontalGroup(
+            deskPnlContenedorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 798, Short.MAX_VALUE)
+        );
+        deskPnlContenedorLayout.setVerticalGroup(
+            deskPnlContenedorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 503, Short.MAX_VALUE)
+        );
+
+        btnMenu.setText("MENU");
+
+        btnPrueba.setText("Prueba");
+        btnPrueba.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPruebaActionPerformed(evt);
+            }
+        });
+        btnMenu.add(btnPrueba);
+
+        menuPrincipal.add(btnMenu);
+
+        setJMenuBar(menuPrincipal);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(deskPnlContenedor)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(deskPnlContenedor)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPruebaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPruebaActionPerformed
+        FrmPrueba fp = new FrmPrueba();
+        deskPnlContenedor.add(fp);
+        fp.setVisible(true);
+    }//GEN-LAST:event_btnPruebaActionPerformed
+
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrmAdministracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrmAdministracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrmAdministracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrmAdministracion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrmAdministracion().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu btnMenu;
+    private javax.swing.JMenuItem btnPrueba;
+    private javax.swing.JDesktopPane deskPnlContenedor;
+    private javax.swing.JMenuBar menuPrincipal;
+    // End of variables declaration//GEN-END:variables
+
+}
