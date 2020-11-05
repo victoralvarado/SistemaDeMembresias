2e89f7d4971cb0cb796ccaf9ff8f70e7be484dc7
EverAlexander <71670356+EverAlexander@users.noreply.github.com>
2020-11-05 15:30:58 -0600

    DaoUsuario
    agregacion de public boolean login

--- SistemaDeMembresias1.0/src/com/dao/DaoUsuario.java
+++ SistemaDeMembresias1.0/src/com/dao/DaoUsuario.java
@@ -3,6 +3,8 @@ package com.dao;
 import com.conexion.Conexion;
 import com.interfaces.OperacionesUsuario;
 import com.modelo.Usuario;
+import com.vistas.FrmAdministracion;
+import com.vistas.FrmPrincipal;
 import java.awt.HeadlessException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -124,4 +126,49 @@ public class DaoUsuario extends Conexion implements OperacionesUsuario{
             this.desconectar();
         }
     }
+
+    @Override
+    public boolean login(Usuario us) throws Exception {
+        ResultSet rs;
+        boolean estado = false;
+        int nivel = 0;
+        FrmAdministracion adm= new FrmAdministracion();
+        FrmPrincipal suscrip =  new FrmPrincipal();
+        try {
+            this.conectar();
+            String sql = "select * from usuario where  email= ? && password = ?";
+            PreparedStatement pre = this.getCon().prepareCall(sql);
+            pre.setString(1, us.getEmail());
+            pre.setString(2, us.getPassword());
+            rs = pre.executeQuery();
+            while (rs.next()) {
+                estado = true;
+                nivel = rs.getInt("tipoUsuario");
+            }
+            if (estado) {
+                if (nivel == 1) {
+                    //Administrador
+                    adm.setVisible(true);
+                }
+                if(nivel == 2){
+                    //Editor
+                    adm.setVisible(true);
+                }
+                if (nivel == 3) {
+                    //Suscriptor
+                    suscrip.setVisible(true);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "UserName y/o password incorrectos",
+                        "Login", JOptionPane.WARNING_MESSAGE);
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.desconectar();
+        }
+        return estado;
+    }
+    
 }
