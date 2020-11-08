f91970313f458ddb5321fbae27fe11ee374e78ea
Sander503 <72053537+Sander503@users.noreply.github.com>
2020-11-08 17:36:56 -0600

    Create OperacionesPublicidad.java

---
+++ SistemaDeMembresias1.0/src/com/interfaces/OperacionesPublicidad.java
@@ -0,0 +1,19 @@
+
+package com.interfaces;
+
+import java.util.List;
+import com.modelo.Publicidad;
+
+/**
+ * Nombre de la interface: OperacionesPublicidad
+ * Fecha: 08/11/2020
+ * Versión: 1.0
+ * CopyRight: ITCA-FEPADE
+ * @author adrian luna
+ */
+public interface OperacionesPublicidad {
+    public List<Publicidad> mostrarPublicidad() throws Exception;
+    public void insertarPublicidad(Publicidad pub) throws Exception;
+    public void modificarPublicidad(Publicidad pub) throws Exception;
+    public void eliminarPublicidad(Publicidad pub) throws Exception;
+}
