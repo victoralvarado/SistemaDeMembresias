0d6e56ce12eedd2c10da86690aa9168798356d9c
Sander503 <72053537+Sander503@users.noreply.github.com>
2020-11-18 16:41:01 -0600

    FrmSociosRegistrados

--- SistemaDeMembresias1.0/src/com/vistas/FrmSociosRegistrado.java
+++ SistemaDeMembresias1.0/src/com/vistas/FrmSociosRegistrado.java
@@ -105,6 +105,7 @@ public class FrmSociosRegistrado extends javax.swing.JInternalFrame {
         jLabel2 = new javax.swing.JLabel();
         dateFechaActual = new com.toedter.calendar.JDateChooser();
         jButton1 = new javax.swing.JButton();
+        btnSuscripcion = new javax.swing.JButton();
 
         setClosable(true);
         setIconifiable(true);
@@ -136,6 +137,13 @@ public class FrmSociosRegistrado extends javax.swing.JInternalFrame {
             }
         });
 
+        btnSuscripcion.setText("Reporte Suscripcion");
+        btnSuscripcion.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnSuscripcionMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -153,23 +161,30 @@ public class FrmSociosRegistrado extends javax.swing.JInternalFrame {
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(dateFechaActual, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(22, 578, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnSuscripcion)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addGap(22, 22, 22))
+                .addGap(25, 25, 25))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(9, 9, 9)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel2)
-                    .addComponent(dateFechaActual, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(dateFechaActual, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(9, 9, 9)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSuscripcion)
                     .addComponent(jButton1))
-                .addGap(29, 29, 29)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(52, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -207,8 +222,22 @@ public class FrmSociosRegistrado extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_jButton1MouseClicked
 
+    private void btnSuscripcionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSuscripcionMouseClicked
+        try {
+            con.conectar();
+            Map parametros = new HashMap();
+            parametros.put("fech", formatoFecha.format(dateFechaActual.getDate()));
+            reporte = JasperCompileManager.compileReport("src/com/reportes/reporteSociosDiario.jrxml");
+            JasperPrint jp = JasperFillManager.fillReport(reporte, parametros, con.getCon());
+            JasperViewer.viewReport(jp,false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_btnSuscripcionMouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSuscripcion;
     private com.toedter.calendar.JDateChooser dateFechaActual;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
