ca43363910d8ade37962c865eeb9cf8af29f816b
AndreRoro <rosalesrojoan@gmail.com>
2020-11-18 22:23:26 -0600

    DaoPersonaExterna

--- SistemaDeMembresias1.0/src/com/dao/DaoPersonaExterna.java
+++ SistemaDeMembresias1.0/src/com/dao/DaoPersonaExterna.java
@@ -71,7 +71,7 @@ public class DaoPersonaExterna extends Conexion implements OperacionesPersonaExt
     public void modificarPersonaExterna(PersonaExterna pe) throws Exception {
         try {
             this.conectar();
-            String sql = "update  personaExterna set nombre = ?, dui = ?, telefonoMovil = ?, idSuscriptor = ? where idPersonaExerna = ?;";
+            String sql = "update  personaExterna set nombre = ?, dui = ?, telefonoMovil = ?, idSuscriptor = ? where idPersonaExterna = ?;";
             PreparedStatement pre = this.getCon().prepareStatement(sql);
             pre.setString(1, pe.getNombre());
             pre.setString(2, pe.getDui());
@@ -91,7 +91,7 @@ public class DaoPersonaExterna extends Conexion implements OperacionesPersonaExt
     public void eliminarPersonaExterna(PersonaExterna pe) throws Exception {
         try {
             this.conectar();
-            String sql = "delete from  personaExterna where idPersonaExerna = ?;";
+            String sql = "delete from  personaExterna where idPersonaExterna = ?;";
             PreparedStatement pre = this.getCon().prepareStatement(sql);
             pre.setInt(1, pe.getIdPersonaExterna());
             pre.executeUpdate();
